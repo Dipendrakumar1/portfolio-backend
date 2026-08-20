3875deb6194000c92d739a353dcab19ea4d51b58
Dipendra Kumar Yadav <dipendrayadav299@gmail.com>
2026-08-20 17:44:49 +0530

    cors changed.

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def load_user(user_id):
 db.init_app(app)
 
 # CORS configuration: Restrict to specific origins
-frontend_url = os.getenv("FRONTEND_URL", "//www.dipendrakumaryadav.com.np")
+frontend_url = os.getenv("FRONTEND_URL", "https://www.dipendrakumaryadav.com.np")
 CORS(app, origins=[frontend_url])
 
 try:
